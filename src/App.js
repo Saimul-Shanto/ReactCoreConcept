13d6e4390e67afc924422eda847506f507a414d1
Saimul-Shanto <saimulhaque@iut-dhaka.edu>
2023-05-26 23:13:10 +0600

    add new component person

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Person></Person>
+      <Person></Person>
+    </div>
+  );
+}
+
+function Person(){
+  return (
+    <h1>Hello Dear!!!</h1>
+  )
+}
+export default App;
